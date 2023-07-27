6af9d2cd05c936b13251705a78eba571739a6e77
Supanut1911 <ynotmela@gmail.com>
2023-07-27 12:49:39 +0700

    add docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { ProfileModule } from './profile/profile.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductsModule, ProfileModule],
+  imports: [
+    ProductsModule,
+    ProfileModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'dev-site-db.cmedbxptmexp.ap-southeast-1.rds.amazonaws.com',
+      port: 5432,
+      username: 'postgres',
+      password: '9u^K354ZLW48FINX',
+      database: 'finweb-strapi-dev-db',
+      entities: [],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
